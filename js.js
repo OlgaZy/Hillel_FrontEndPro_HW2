6d298b97796badb56be64a4b534a7f60c48d461e
Olga <olgazykova27@gmail.com>
2024-12-03 22:49:31 +0200

    data types
    output data types

--- js.js
+++ js.js
@@ -1,2 +1,33 @@
-var number = 50;
+let number = 50.5;
 console.log(typeof number);
+
+let string = "Hello, I'm a string!"
+console.log(typeof string); 
+
+let bool = true;
+console.log(typeof bool); 
+
+let underf;
+console.log(typeof underf); 
+
+let empty = null;
+console.log(typeof empty); 
+
+let uniq_id = Symbol("id123")
+console.log(typeof uniq_id); 
+
+let big_number = 1234567890123456789012345678901234567890n;
+console.log(typeof big_number); 
+
+let mass = [23, 45, 43]
+console.log(typeof mass);
+
+let list =  { first_name: "Alex", last_name: "Brown",  age: 29 }
+console.log(typeof list); 
+
+console.log(typeof function() {}); 
+
+let today = new Date();
+console.log(typeof today);
+
+console.log(typeof some_vriable);
